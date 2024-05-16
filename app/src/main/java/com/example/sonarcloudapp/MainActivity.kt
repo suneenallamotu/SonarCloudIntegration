6a302aa1847dc6f5e5d06bbd4664445a951bffd3
Suneetha Nallamotu <suneethanallamotu@gmail.com>
2024-05-15 20:18:02 -0700

    added image and custom text style

--- app/src/main/java/com/example/sonarcloudapp/MainActivity.kt
+++ app/src/main/java/com/example/sonarcloudapp/MainActivity.kt
@@ -4,30 +4,102 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sonarcloudapp.ui.theme.SonarCloudAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
+        )
         setContent {
             SonarCloudAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
-                Button(onClick = {
-
-                }) {
-                    Text(text = "Start Run")
+                val painter = painterResource(id = R.drawable.image1)
+                val description = "Duck in the farm"
+                val title = "Duck title"
+                Box(modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+                ){
+                    ImageCard(painter = painter,
+                        contentDescription = description,
+                        title = title)
                 }
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
+
+                
+//                Button(onClick = {
+//
+//                }) {
+//                    Text(text = "Start Run")
+//                }
             }
         }
     }
@@ -39,6 +111,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+       // elevation =  CardElevation = CardDefaults.cardElevation(),
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(painter = painter, contentDescription = contentDescription,
+                contentScale = ContentScale.Crop)
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f,
+
+                        )
+                )
+                )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
